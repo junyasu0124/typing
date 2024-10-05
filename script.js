206b4727309d2ddc8be1973cc537974121e6197c
junyasu <jumpeiyasue@gmail.com>
2024-10-06 03:21:45 +0900

    feat

---
+++ script.js
@@ -0,0 +1,23 @@
+let startPage;
+let playPage;
+let endPage;
+
+window.onload = () => {
+  startPage = document.getElementById('startPage');
+  playPage = document.getElementById('playPage');
+  endPage = document.getElementById('endPage');
+
+  start();
+}
+
+function start() {
+
+}
+
+function play() {
+
+}
+
+function end() {
+
+}
